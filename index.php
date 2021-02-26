408766f1eacc451547fd106ce8f3f12f1a6620f4
williams23 <velandiaw23@hotmail.com>
2021-02-25 23:48:04 -0500

    taller

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+  //  1.	Programa que lea un valor de precio base y calcule su impuesto, 
+  //  si es valor es mayor de 150.000 se le aplicará un 19% de impuestos, 
+  //  en caso contrario se le aplicará un 16%.
+    $a=150000;
+    $b=16;
+    
+    $porcent = ($a*$b/100);
+
+
+
+    if ($a > $b) {
+        echo " ";
+    } elseif ($a == $b) {
+        echo "a";
+    } else {
+        echo " b";
+    }
+    ?>
+  
+
+
+?>
